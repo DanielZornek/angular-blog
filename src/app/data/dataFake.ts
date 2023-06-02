4e05b93d274e8a992c8c2ab0937ac888cd8f271b
DanielZornek <daniel_zornek91@outlook.com>
2023-06-01 23:23:22 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Dabi é Touya Todoroki!!",
+        "description": "O vilão Dabi revelou sua verdadeira identidade, mostrando ser progenitor do herói número 1 Endeavor, o que será vem por aí.",
+        "photCover": "https://elvortex.com/wp-content/uploads/2020/11/touya-es-dabi.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Aprenda a fazer churrasco do jeito certo com Dabi",
+        "description": "O mestre das Chamas Toya Todoroki irá nos ensinar a fazer aquela picanha deliciosa pro fim de semana com a rapaziada.",
+        "photCover": "https://anime.atsit.in/wp-content/uploads/2022/12/dabi-de-my-hero-acadekaren-e-eleito-o-vilao-mais-valioso-para-uma-grande-revelacao-no-episodio-11-e-seu-discurso-brutal-ganha-um-video-especial.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Uma conversa sobre amor com Himiko Toga - Love Talks Night",
+        "description": "Aprendendo mais sobre amor com nossa psicopata apaixonada.",
+        "photCover": "https://nerdhits.com.br/wp-content/uploads/2021/11/himiko-toga-my-hero-academia.jpg"
+    },
+    {
+        "id": "4",
+        "title": "'Eu não tenho preconceito, só odeio que respirem' - comenta Tomura Shigaraki sobre os seres humanos",
+        "description": "Polêmico!!!!",
+        "photCover": "https://kanto.legiaodosherois.com.br/w728-h381-gnw-cfill-gcc/wp-content/uploads/2021/10/legiao_cigUOujGdvNh.jpg.webp"
+    }
+];
